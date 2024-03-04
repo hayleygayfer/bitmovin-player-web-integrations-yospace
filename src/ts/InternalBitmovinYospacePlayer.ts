7f0a6061fb94c0411df0820a8e73e026a0a7b330
Hayley Gayfer <hgayfer@nine.com.au>
2024-03-05 09:13:38 +1100

    Removed code to isolate metadata reporting to live

--- src/ts/InternalBitmovinYospacePlayer.ts
+++ src/ts/InternalBitmovinYospacePlayer.ts
@@ -1100,9 +1100,7 @@ export class InternalBitmovinYospacePlayer implements BitmovinYospacePlayerAPI {
     this.player.on(this.player.exports.PlayerEvent.ViewModeChanged, this.onViewSizeChanged);
 
     // To support ads in live streams we need to track metadata events
-    if (this.yospaceSourceConfig.assetType === YospaceAssetType.LINEAR) {
-      this.player.on(this.player.exports.PlayerEvent.Metadata, this.onMetaData);
-    }
+    this.player.on(this.player.exports.PlayerEvent.Metadata, this.onMetaData);
   }
 
   private unregisterPlayerEvents(): void {
@@ -1120,9 +1118,7 @@ export class InternalBitmovinYospacePlayer implements BitmovinYospacePlayerAPI {
     this.player.off(this.player.exports.PlayerEvent.ViewModeChanged, this.onViewSizeChanged);
 
     // To support ads in live streams we need to track metadata events
-    if (this.yospaceSourceConfig.assetType === YospaceAssetType.LINEAR) {
-      this.player.on(this.player.exports.PlayerEvent.Metadata, this.onMetaData);
-    }
+    this.player.on(this.player.exports.PlayerEvent.Metadata, this.onMetaData);
   }
 
   private onPlaying = () => {
